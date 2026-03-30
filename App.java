11d7a751073053b14d79eab8220c0d47f5814aed
Heidy Mendoza <heidydml29@gmail.com>
2026-03-29 21:13:44 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,18 @@
+/*  Ejercio 1
+    Heidy Mendoza
+    Enunciado: Crear una clase llamada Mascota en Java que contenga los atributos nombre, especie y edad. 
+    Implementar un constructor para inicializar estos datos y un método mostrarInfo() 
+    que imprima la información de la mascota. */
+
+public class App {
+
+    public static void main(String[] args) {
+        // Objetos de tipo Mascota
+        Mascota mascota1 = new Mascota("Firulais", "Perro", 3);
+        Mascota mascota2 = new Mascota("Michi", "Gato", 2);
+
+        // Mostrar información de las mascotas
+        mascota1.mostrarInfo();
+        mascota2.mostrarInfo();
+    }
+}
